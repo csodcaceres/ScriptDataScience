1b8220600b1e00f9a252b9cdcfb2798a291306ac
csodcaceres <cs.odcaceres@gmail.com>
2025-04-22 12:32:31 -0300

    Numero random de una lista

---
+++ listaDeNumeros.py
@@ -0,0 +1,8 @@
+# Script que lee una lista de numeros y elije uno al azar
+
+import random
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+
+numero_random = random.choice(lista)
+print(numero_random)
